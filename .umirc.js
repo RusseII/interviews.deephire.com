e16ed5f91123f9b260a53c89c47f013888771623
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-02-06 06:13:16 -0500

    UmiJs scaffoldig

---
+++ .umirc.js
@@ -0,0 +1,21 @@
+
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: false,
+      dynamicImport: false,
+      title: 'umi',
+      dll: false,
+      routes: {
+        exclude: [
+        
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
