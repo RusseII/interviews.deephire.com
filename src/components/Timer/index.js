9dcc8baa840b8f4c52109ba82e7599accfe1e240
Russell <russell.ratcliffe@captitalone.com>
2019-02-25 09:27:27 -0500

    add timer

---
+++ src/components/Timer/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Progress } from 'antd';
+
+const Timer = ({countDown, seconds, onFinish, paused}) => {
+  const [time, setTime] = useState(seconds);
+
+  const timer = setTimeout(function() {
+    paused ? console.log("paused"): setTime(time - 1);
+  }, 1000);
+
+  if (time === 0) {
+      clearTimeout(timer);
+      onFinish()
+  }
+
+  return (
+    <div>
+        { countDown ? 
+      <Progress width="75px" type="circle" format={percent => time} percent={(time/seconds) * 100} /> :
+      <Progress width="75px" type="circle" format={percent => seconds - time + " sec" } percent={(1 - time/seconds) * 100} />
+  }
+
+    </div>
+  );
+};
+
+export default Timer;
--- src/pages/record.js
+++ src/pages/record.js
@@ -1,43 +1,35 @@
 import data from './temp.js';
-import React, { useEffect, useState, useReducer, useRef} from 'react';
+import React, { useState } from 'react';
 
 import ReactPlayer from 'react-player';
-import { Button, Progress, Row, Col } from 'antd';
-import styles from './index.less';
+import { Button, Row, Col } from 'antd';
+import styles from './record.less';
 
 import { camerakit } from './assets/camerakit-web.min.js';
-import vimeo from "./vimeo.js"
+import vimeo from './vimeo.js';
+import Timer from '@/components/Timer';
 
 let myStream;
 export default () => {
+  const [before, setBefore] = useState(true);
   const { interview_config: interviewConfig, interview_questions: interviewQuestions } = data.data;
   const { answerTime, prepTime, retakesAllowed } = interviewConfig;
   const [videoUrl, setUrl] = useState();
 
-
-const setUrlHack = (objectURL) => {
+  const setUrlHack = objectURL => {
     // I have no idea why I have to do this, if i just call setURL directly, the
     // url is not changed and it still renders the mediastream obj
-setUrl("")
-setTimeout(function(){    setUrl(objectURL)
-}, 1);
+    setUrl('');
+    setTimeout(function() {
+      setUrl(objectURL);
+    }, 1);
+  };
 
-}
-
-
-
-  const [count, setCount] = useState(50);
   const stop = () => {
-    const recordedVideo = myStream.recorder.stop(); // Use the video yourself
+    const recordedVideo = myStream.recorder.stop();
     const objectURL = URL.createObjectURL(recordedVideo);
-    // myStream.recorder.downloadLatestRecording(); // Download the video direct from browser
-
-    // Stop using camera
     myStream.destroy();
-    setUrlHack(objectURL)
-    // vimeo(recordedVideo)
-
-
+    setUrlHack(objectURL);
   };
 
   const start = async () => {
@@ -47,13 +39,11 @@ setTimeout(function(){    setUrl(objectURL)
       audio: devices.audio[0],
       video: devices.video[0],
     });
-    
 
     myStream.setResolution({ aspect: 16 / 9 });
     myStream.recorder.start();
     const streamUrl = await myStream.getMediaStream();
     setUrl(streamUrl);
-    
   };
 
   return (
@@ -62,20 +52,45 @@ setTimeout(function(){    setUrl(objectURL)
         {' '}
         <h1>Start Your Practice Interview</h1>{' '}
       </div>
+      <Timer countDown={true} paused onFinish={() => console.log('finished')} seconds={prepTime} />
+      <br />
 
       <Row type="flex" justify="center">
         <Col span={15}>
-          <div className={styles.playerWrapper}>
-            <ReactPlayer 
-            className={styles.reactPlayer} controls playing url={videoUrl} width="100%" height="100%" />
-          </div>
+          {before ? (
+            <div>
+              Welcome to your video Interview. You will be presented 5 different questions, with 40
+              seconds to prepare for each question, 90 seconds to answer each question, and 8
+              retakes throughout the entire interview. If you have any questins about the
+              experience, send me a message on the bottom right.
+            </div>
+          ) : (
+            <div className={styles.playerWrapper}>
+              <ReactPlayer
+                className={styles.reactPlayer}
+                controls
+                playing
+                url={videoUrl}
+                width="100%"
+                height="100%"
+              />
+            </div>
+          )}
         </Col>
       </Row>
-      <Progress type="circle" status="exception" format={percent => percent} percent={count} />
-      <Button onClick={start}>start</Button>
-      <Button onClick={stop}>stop</Button>
-      <Button onClick={onclick}>rend</Button>
+      {before ? (
+        <Button className={styles.button} onClick={() => setBefore(false)}>
+          Start Pracice Interview
+        </Button>
+      ) : (
+        <>
+          <Button onClick={start}>start</Button>
+          <Button onClick={stop}>stop</Button>
+        </>
+      )}
 
+      {/* <Button onClick={start}>start</Button>
+      <Button onClick={stop}>stop</Button> */}
     </div>
   );
 };
