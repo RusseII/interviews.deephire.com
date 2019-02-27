15b06582e4e8278b796b0c0e778ccfbded3d3dba
Russell <russell.ratcliffe@captitalone.com>
2019-02-27 09:33:47 -0500

    improve timer component

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -3,9 +3,9 @@ import { Progress } from 'antd';
 
 const Timer = ({countDown, seconds, onFinish, paused}) => {
   const [time, setTime] = useState(seconds);
-
+ 
   const timer = setTimeout(function() {
-    paused ? console.log("paused"): setTime(time - 1);
+    if (!paused) setTime(time - 1);
   }, 1000);
 
   if (time === 0) {
@@ -19,7 +19,6 @@ const Timer = ({countDown, seconds, onFinish, paused}) => {
       <Progress width="75px" type="circle" format={percent => time} percent={(time/seconds) * 100} /> :
       <Progress width="75px" type="circle" format={percent => seconds - time + " sec" } percent={(1 - time/seconds) * 100} />
   }
-
     </div>
   );
 };
