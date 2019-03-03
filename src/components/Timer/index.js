9470e0acf7e217a6939bb0cd4b5f3f494397c9af
Russell <russell.ratcliffe@captitalone.com>
2019-03-03 17:44:31 -0500

    center interview img + padding for timer

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -14,7 +14,7 @@ const Timer = ({countDown, seconds, onFinish, paused}) => {
   }
 
   return (
-    <div>
+    <div style={{paddingTop: "24px"}}>
         { countDown ? 
       <Progress width="75px" type="circle" format={percent => time} percent={(time/seconds) * 100} /> :
       <Progress width="75px" type="circle" format={percent => seconds - time + " sec" } percent={(1 - time/seconds) * 100} />
