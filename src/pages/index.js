10bdbcf3403671c958703a5531004958e7d4ee3f
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-03-06 08:51:52 -0500

    add iosmodal to first screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import styles from './index.less';
 import ReactPlayer from 'react-player';
-import { Row, Col } from 'antd';
+import { Button, Steps, Modal,Row, Col } from 'antd';
 import SignIn from '@/components/SignIn';
+import qs from 'qs';
 
-const Index = ({location}) => (
+const Step = Steps.Step;
+
+
+const Index = ({location}) => {
+  const pin = qs.parse(location.search)['pin'];
+
+  const downloadiOS = () => window.open("https://itunes.apple.com/us/app/deephire/id1380277628?mt=8", "_blank");
+
+  const [iOS, setIOS] = useState(!!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform));
+    // const [iOS, setIOS] = useState(true);
+
+return (
   <div className={styles.normal}>
+    <Modal
+      closable={false}
+      title="Please download our mobile app to Interview on IOS"
+      visible={iOS}
+      footer={[
+        <Button key="Submit" type="primary" onClick={window.openChat}>
+          Contact Support
+            </Button>,
+      ]}
+    // onCancel={() => setIOS(false)}
+    >
+
+      <Steps
+        direction="vertical"
+        size="small"
+        current={0}
+      >
+        <Step
+          title="Download App"
+          description={
+            <div>
+              <Button
+                onClick={downloadiOS}
+                type="secondary"
+              >
+                iOS
+    </Button>{" "}
+
+            </div>
+          }
+        />
+        <Step
+          title="Enter Code"
+          description={<div className="content" dangerouslySetInnerHTML={{ __html: "Your interview code is <h3 ><b>" + pin + "</b></h3>" }}></div>}
+        // description={"Your"+ <b>hi</b> +"interview code is " + this.state.pin}
+        // description=
+
+        />
+        <Step title="Take the interview!" description="Good luck!" />
+      </Steps>
+    </Modal>
     <div style={{ paddingTop: '24px' }}>
       <h1>Welcome to your Video Interview!</h1>{' '}
     </div>
@@ -24,6 +77,6 @@ const Index = ({location}) => (
     </Row>
     <SignIn location={location}/>
   </div>
-);
+)};
 
 export default Index;
