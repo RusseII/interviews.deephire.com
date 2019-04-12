c7888a859d4241d0332dddce5f551df12eb83e33
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-04-12 17:23:15 -0400

    fix typo

--- src/pages/record.js
+++ src/pages/record.js
@@ -298,7 +298,7 @@ export default ({ location }) => {
             setInterview({ ...interview, helperText: 'Prepare your answer', paused: false });
           }}
         >
-          Start Pracice Interview
+          Start Practice Interview
         </Button>
       ) : (
         <>
