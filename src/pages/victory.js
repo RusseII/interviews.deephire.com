c234fafcf2804efd0786bfd87e8523f4099694fa
russell <russell@deephire.com>
2020-07-29 14:05:57 -0500

    imrpove victory screen

--- src/pages/victory.js
+++ src/pages/victory.js
@@ -1,22 +1,45 @@
 
-import { openChat } from '@/services/crisp';
-import { Button, Row } from 'antd';
-import React from 'react';
-import { lowerCaseQueryParams } from '@/services/helpers.js';
+import { Button, Row, Rate, Form, Switch, Input, Typography } from 'antd';
+import React, { useState } from 'react';
 import styles from './victory.less';
 
 export default () => {
-  const { chat } = lowerCaseQueryParams(window.location.search);
+
+  const [finished, setFinished] = useState(false)
   
+  const layout = {
+    labelCol: { span: 10 },
+    wrapperCol: { span: 6 },
+  };
+
+  const onFinish = (values) => {
+    console.log("finish")
+    console.log(values)
+    setFinished(true)
+  }
+
+  if (finished) return <Success/>
+
   return (
     <div className={styles.wrapper}>
-      <div style={{ paddingTop: '24px' }}>
-        <h1>Interview Completed!</h1>
-        Your Interview has been successfully submitted
-      </div>
-
-      <br />
-
+      <div style={{textAlign: 'center', marginBottom: 48}}>
+ <Typography.Title>One Last Step</Typography.Title>
+ <Typography.Text type='secondary'>All feedback is anonymous</Typography.Text>
+ </div>
+      <Form onFinishFailed={(fail) => console.log(fail)} onFinish={onFinish} {...layout}>
+        <Form.Item  label="How would you rate the video introduction software?"
+        name="rating"
+        > <Rate/></Form.Item>
+        <Form.Item name="issues" label="Did you have any software issues?"><Switch checkedChildren="Yes" unCheckedChildren="No"  /></Form.Item>
+        <Form.Item name='ratingReason' label="Why did you give the rating above?"><Input.TextArea/></Form.Item>
+        <Form.Item  label="Would you do another video intro like this in the future?"
+        name="anotherIntervieew"
+        > <Switch checkedChildren="Yes" unCheckedChildren="No" defaultChecked /></Form.Item>
+        <Form.Item  wrapperCol={{ xs: {span: 24 , offset: 0 }, sm: {span: 12 , offset:10 }} }><Button  type="primary" htmlType="submit">Complete Video Introduction</Button></Form.Item>
+        
+      </Form>
+     
+{/* 
       <Row type="flex" justify="center">
         <div className="OTPublisherContainer" style={{ overflow: 'hidden' }}>
           <img
@@ -26,12 +49,21 @@ export default () => {
             alt="Prepare to Record!"
           />
         </div>
-      </Row>
-      {chat !== '0' && (
-        <Button type="primary" onClick={openChat} className={styles.button}>
-          Leave Feedback
-        </Button>
-      )}
+      </Row>*/}
     </div>
   );
 };
+
+
+const Success = () => (
+  <Row type="flex" justify="center">
+  <div className="OTPublisherContainer" style={{ overflow: 'hidden' }}>
+    <img
+      style={{ margin: '0 -100%', verticalAlign: 'middle' }}
+      height="100%"
+      src="https://deephire.s3.amazonaws.com/logos/trophy.gif"
+      alt="Prepare to Record!"
+    />
+      </div>
+      </Row>
+)
